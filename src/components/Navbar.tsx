a8eb760f477abd23cbfc65a2377c93163319f445
umer riaz <umer.riaz@solutionsurface.com>
2023-06-07 11:50:54 +0500

    changed logo based on colormode

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,10 @@
-import { Grid, Image } from '@chakra-ui/react';
-import logo from '../assets/logoLight.png';
+import { Grid, Image, useColorMode } from '@chakra-ui/react';
+import logoLight from '../assets/logoLight.png';
+import logoDark from '../assets/logoDark.png';
 import NavbarMenu from './NavbarMenu';
 const Navbar = () => {
+  const {colorMode} = useColorMode();
+  const logo = colorMode === 'dark'? logoDark:logoLight;
   return (
     <Grid templateColumns="1fr 1fr">
         <Image src={logo} alt='Umer Riaz' boxSize='100px' objectFit='contain'/>
